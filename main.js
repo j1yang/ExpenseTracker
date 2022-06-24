2bdc70eae461272a30de1f0387ad7c3af144e6bd
j1yang <106880500+j1yang@users.noreply.github.com>
2022-06-24 10:04:30 -0400

    JS: BtnClose

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+// Btn add-transaction clicked, 
+// scroll down to add transaction section
 const BtnAddTransaction = document.querySelector('.transactions__add-btn');
 const sectionAddTxn = document.querySelector('#add-transaction');
 BtnAddTransaction.addEventListener('click', ()=>{
   sectionAddTxn.classList.add('visible');
   scrollIntoView('#add-transaction');
   });
+
+
+// BtnClose click, close add-transaction section and 
+// scroll up to transactions
+const btnClose = document.querySelector('.add-transaction__close');
+btnClose.addEventListener('click', ()=>{
+  scrollIntoView('#transactions');
+  sectionAddTxn.classList.remove('visible');
+});
 
 function scrollIntoView(selector){
   const scrollTo = document.querySelector(selector);
